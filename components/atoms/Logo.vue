35c043a7621667a02644489e2e74f6df672fce9b
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-08 05:20:37 -0300

    feat(layout): create ibook layout

---
+++ components/atoms/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo">
+    <img src="@/assets/images/logo.svg" alt="iBook" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+}
+</style>
