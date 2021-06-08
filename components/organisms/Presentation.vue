099022bc4ca11a6227167186843e324bd684bd32
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-08 05:43:06 -0300

    feat(page): create About page

---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="presentation">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt
+      libero quis, temporibus nam a nostrum, omnis eveniet atque accusamus illo
+      modi impedit inventore. Dolores a placeat provident voluptatem et!
+      Eligendi debitis, dolorum consectetur quisquam rem suscipit quaerat illo
+      eum, praesentium unde consequatur pariatur eius sapiente officia deleniti
+      magni placeat doloribus. Cum hic doloribus laborum accusantium impedit
+      amet illum illo.
+    </p>
+
+    <img src="@/assets/images/presentation.svg" alt="Apresentação" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+
+  @include screen("small", "medium") {
+    grid-template-columns: 1fr;
+  }
+
+  p {
+    line-height: 26px;
+  }
+
+  img {
+    max-width: 100%;
+  }
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="box">
+    <AboutTemplate />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  layout: "ibook",
+});
+</script>
