52e0707b7cab22150fa7d2694177bf7869aa3705
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-08 03:46:43 -0300

    feat(head): add title, lang and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ibook",
+    title: "iBook",
     htmlAttrs: {
-      lang: "en",
+      lang: "pt-BR",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Uma nova maneira de ler livros online!",
+      },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
