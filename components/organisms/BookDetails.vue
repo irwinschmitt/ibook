7f56f63868432cdb237296448f2290fff17b5d3a
Irwin Schmitt <irwinschmitt@gmail.com>
2021-06-08 23:03:04 -0300

    feat(page): create book details page

---
+++ components/organisms/BookDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="book-details">
+    <BookCover />
+    <BookInfo />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.book-details {
+  display: grid;
+  grid-template-columns: 250px 1fr;
+  grid-gap: 1.4rem;
+
+  @include screen("medium", "small") {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
